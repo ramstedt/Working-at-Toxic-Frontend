ea08525e013d0f4a3af3d47d16d11657f373797f
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2024-11-18 19:43:48 +0100

    add environment key + connection to API
    - Added the API key + env example file
    - Established a connection to the movie database API.
    - Verified the connection with a fetch request.

---
+++ toxic-frontend/lib/themoviedb.js
@@ -0,0 +1,29 @@
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = process.env.NEXT_PUBLIC_THEMOVIEDB_API_KEY;
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+  },
+};
+
+export const fetchPopularTVShows = async () => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/tv/popular?api_key=${API_KEY}`,
+      options
+    );
+
+    if (!response.ok) {
+      throw new Error(
+        `Error fetching popular TV shows: ${response.statusText}`
+      );
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Failed to fetch popular TV shows:', error);
+    throw error;
+  }
+};
