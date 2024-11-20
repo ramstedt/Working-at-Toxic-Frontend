d7c7e833984e8e56d1a766c561c7e5f8df11aa2d
Emma Ramstedt <71063351+emmaramstedt@users.noreply.github.com>
2024-11-20 15:35:23 +0100

    style fixes
    some minor style fixes

--- toxic-frontend/app/components/_atoms/SearchButton/SearchButton.jsx
+++ toxic-frontend/app/components/_atoms/SearchButton/SearchButton.jsx
@@ -1,5 +1,9 @@
 import styles from './SearchButton.module.css';
 
-export default function SearchButton({ text }) {
-  return <button className={styles.searchButton}>{text}</button>;
+export default function SearchButton({ text, onClick }) {
+  return (
+    <button onClick={onClick} className={styles.searchButton}>
+      {text}
+    </button>
+  );
 }
